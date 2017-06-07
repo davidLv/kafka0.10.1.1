63a193299b277364e1e3ca428eea398215b35274
zhangsulei <youtong82@163.com>
2017-06-07 11:06:00 +0800

    1

--- src/main/java/com/cdel/stream/wordcount2/WordCountLambdaExample.java
+++ src/main/java/com/cdel/stream/wordcount2/WordCountLambdaExample.java
@@ -2,7 +2,7 @@ package com.cdel.stream.wordcount2;
 
 import java.util.Arrays;
 import java.util.Properties;
-
+import static java.util.stream.Collectors.toList;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
@@ -10,9 +10,12 @@ import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KStreamBuilder;
+import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.processor.WallclockTimestampExtractor;
 
 //http://www.orchome.com/335
+//导出jar包放到fakfa服务器执行，不知道为什么本地执行报错：
+//Exception in thread "StreamThread-1" java.lang.UnsatisfiedLinkError: C:\Users\dell\AppData\Local\Temp\librocksdbjni6765188753473050018.dll: Can't find dependent libraries
 public class WordCountLambdaExample {
 
 	public static void main(String[] args) {
@@ -20,35 +23,28 @@ public class WordCountLambdaExample {
 		KStreamBuilder builder = new KStreamBuilder();
 
 		//序列化/反序列化Sting和Long类型
-		final Serde<String> stringSerde = Serdes.String();
-		final Serde<Integer> longSerde = Serdes.Integer();
+		final Serde<byte[]> byteSerde = Serdes.ByteArray();
+		final Serde<Long> longSerde = Serdes.Long();
 
 		//通过指定输入topic “streams-file-input”来构造KStream实例，
 		//输入数据就以文本的形式保存在topic “streams-file-input” 中。
 		//(在本示例中，我们忽略所有消息的key.)
-		KStream<String, String> textLines = builder.stream(stringSerde, stringSerde, "qz");
+		KStream<byte[], byte[]> textLines = builder.stream(byteSerde, byteSerde, "qz");
 		textLines
 		//以空格为分隔符，将每行文本数据拆分成多个单词。
 		//这些文本行就是从输入topic中读到的每行消息的Value。
 		//我们使用flatMapValues方法来处理每个消息Value，而不是更通用的flatMap
 		.flatMapValues(value -> {
 			System.out.println(value);
-			return Arrays.asList(value.toLowerCase().split("\\W+"));
+			String [] arr = new String(value).toLowerCase().split("\\W+");
+			return Arrays.asList(arr).stream().map(str -> str.getBytes()).collect(toList());
 		})
-		//所以我们将每个单词作为map的key。
-		.map((key, value) -> {
-			System.out.println(key);
-			System.out.println(value);
-			return new KeyValue<>(value, value);
-		})
-		.to(stringSerde, stringSerde, "qz2");
-		//通过key来统计每个单词的次数
-		//这会将流类型从KStream<String,String>转为KTable<String,Long> (word-count).
-		//因此我们必须提供String和long的序列化反序列化方法。
-		//.groupByKey().count("Counts")
-		//转化KTable<String,Long>到KStream<String,Long>
-		//.toStream();
-		//将KStream<String,Long>写入到输出topic中。
+		//将单词分组后作为消息的key
+		.groupBy((key, value) -> value)
+		// 统计每个单词出现的次数（即消息的key）
+		.count("Counts")
+		.to(byteSerde, longSerde, "qz2");
+		//将KTable<String,Long>写入到输出topic中。
 		//wordCounts.to(stringSerde, longSerde, "qz2");
 
 		System.out.println("Starting PurchaseProcessor Example");
